35cc88148c1caf49ef423691c72c0415ecfea5c8
Mike <63655075+MixelTe@users.noreply.github.com>
2020-06-06 23:21:36 +0300

    buttons EventListeners

--- ts/main.ts
+++ ts/main.ts
@@ -1,23 +1,19 @@
-const a = document.getElementById("one");
-if (a != null) a.addEventListener("click", btnClick);
+// document.body.addEventListener("click", function (e: MouseEvent)
+// {
+// 	const el = e.target;
 
-const b = document.getElementById("two");
-if (b != null) b.addEventListener("click", btnClick);
+// 	if (el instanceof HTMLButtonElement)
+// 	{
+// 		const rect = { x: el.offsetLeft, y: el.offsetTop, width: el.offsetWidth - 6, height: el.offsetHeight };
+// 		colorPicker.openMenu(rect);
+// 	}
+// });
 
-const c = document.getElementById("three");
-if (c != null) c.addEventListener("click", btnClick);
-
-const d = document.getElementById("four");
-if (d != null) d.addEventListener("click", btnClick);
-
-const e = document.getElementById("five");
-if (e != null) e.addEventListener("click", btnClick);
-
-const f = document.getElementById("six");
-if (f != null) f.addEventListener("click", btnClick);
-
-const g = document.getElementById("seven");
-if (g != null) g.addEventListener("click", btnClick);
+document.body.querySelectorAll("button")
+	.forEach(el =>
+	{
+		el.addEventListener("click", btnClick);
+	});
 
 const colorPicker = new ColorPicker();
 // colorPicker.setStyle({window: {text: "blue"}});
